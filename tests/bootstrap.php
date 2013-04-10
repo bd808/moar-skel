cacfe78052923b94c8d02779650de9eaa973e071
Bryan Davis <bd808@bd808.com>
2013-04-10 10:15:11 -0600

    Use foreman style .env file for 12factor test config.
    Add processing to tests/bootstrap.php that looks for a .env file in the top
    level project directory and loads settings found there using logic compatible
    with heroku's foreman application. This allows easy 12factor style
    configuration injection for the test suite.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,22 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+
+// Twelve-Factor App configuration
+// populate $_ENV from .env if present
+if (is_readable(__DIR__ . '/../.env')) {
+  $lines = explode("\n", file_get_contents(__DIR__ . '/../.env'));
+  foreach ($lines as $line) {
+    // variable extraction logic lifted from
+    // https://github.com/bkeepers/dotenv
+    if (preg_match('/\A(?:export\s+)?(\w+)(?:=|: ?)(.*)\z/', $line, $group)) {
+      $key = $group[1];
+      $val = $group[2];
+      if (preg_match('/\A\'(.*)\'\z/', $val, $group)) {
+        $val = $group[1];
+      } else if (preg_match('/\A"(.*)"\z/', $val, $group)) {
+        $val = $group[1];
+      }
+      $_ENV[$key] = $val;
+    }
+  }
+}
