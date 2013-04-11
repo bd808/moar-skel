d7f8b63521522fa29ea4264daa55c289e5c129d7
Bryan Davis <bd808@bd808.com>
2013-04-11 13:24:08 -0600

    Make .env bootstrapping work with getenv().

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 // populate $_ENV from .env if present
 if (is_readable(__DIR__ . '/../.env')) {
   $lines = explode("\n", file_get_contents(__DIR__ . '/../.env'));
+
   foreach ($lines as $line) {
     // variable extraction logic lifted from
     // https://github.com/bkeepers/dotenv
@@ -16,7 +17,10 @@ if (is_readable(__DIR__ . '/../.env')) {
       } else if (preg_match('/\A"(.*)"\z/', $val, $group)) {
         $val = $group[1];
       }
+      // store in super global
       $_ENV[$key] = $val;
+      // also store in process env vars
+      putenv("{$key}={$val}");
     }
-  }
-}
+  } //end foreach line
+} //end if .env
